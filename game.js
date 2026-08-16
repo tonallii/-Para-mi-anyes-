707fbc93cfa98076cc7dfd52bc41ef0abf8ed26e
tonallii <tonalli.p.avendano@gmail.com>
2026-08-15 22:07:28 -0600

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+<body>
+    <script>
+  function mostrarOcultarTexto() {
+      var texto = document.getElementById("textoOculto");
+      if (texto.style.display === "none") {
+          texto.style.display = "block";
+      } else {
+          texto.style.display = "none";
+      }
+  }
+  </script>
+</body>
+  function mostrarOcultarTexto() {
+      var texto = document.getElementById("textoOculto");
+      if (texto.style.display === "none") {
+          texto.style.display = "block";
+      } else {
+          texto.style.display = "none";
+      }
+  }
+  </script>
